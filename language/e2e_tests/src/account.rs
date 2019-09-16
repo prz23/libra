cbdcbe7a939009186b39a033c98cd28680031fc0
prz23 <panruizheng@zju.edu.cn>
2019-09-16 17:04:37 +0800

    1

--- language/e2e_tests/src/account.rs
+++ language/e2e_tests/src/account.rs
@@ -20,7 +20,7 @@ use types::{
 use vm_genesis::GENESIS_KEYPAIR;
 use vm_runtime::identifier::create_access_path;
 use vm_runtime_types::value::{MutVal, Value};
-
+use std::convert::TryFrom;
 // StdLib account, it is where the code is and needed to make access path to Account resources
 lazy_static! {
     static ref STDLIB_ADDRESS: AccountAddress = { account_config::core_code_address() };
@@ -61,7 +61,7 @@ impl Account {
     pub fn fixed_new_account() -> Self {
         let vec =  [77u8, 180, 239, 25, 146, 136, 157, 68
             , 40, 228, 0, 190, 52, 40, 132, 61, 182, 232, 155, 178, 232, 170, 244, 206, 142, 254, 0, 223, 100, 1, 37, 68];
-        let privkey:Ed25519PrivateKey = Ed25519PrivateKey::try_from(vec);
+        let privkey:Ed25519PrivateKey = Ed25519PrivateKey::try_from(&vec);
         let pubkey:Ed25519PublicKey = Ed25519PublicKey::from(&privkey.clone());
         //here is a  key pair
         Self::with_keypair(privkey, pubkey)
