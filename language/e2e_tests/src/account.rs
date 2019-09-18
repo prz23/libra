2f5d73ca863c7c20781284fe106f4248c816ad01
prz23 <panruizheng@zju.edu.cn>
2019-09-18 10:03:44 +0800

    add account addr

--- language/e2e_tests/src/account.rs
+++ language/e2e_tests/src/account.rs
@@ -69,6 +69,17 @@ impl Account {
         Self::with_keypair(privkey, pubkey)
     }
 
+    pub fn assoc_new_account() -> Self {
+        let vec =  [77u8, 180, 239, 25, 146, 136, 157, 68
+            , 40, 228, 0, 190, 52, 40, 132, 61, 182, 232, 155, 178, 232, 170, 244, 206, 142, 254, 0, 223, 100, 1, 37, 68];
+
+        let (privkey,pubkey) = compat::generate_fixed_assocation_account(vec);
+        //here is a  key pair
+        let mut account = Self::with_keypair(privkey, pubkey);
+        account.addr = account_config::association_address();
+        account
+    }
+
 
     /// Creates a new account with the given keypair.
     ///
